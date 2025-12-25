aae21257a1ee23558b19046f6ad1f98f97cdea8d
Ayusekarazzkya <Azzkyasekarayu@gmail.com>
2025-12-25 17:20:20 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init({
+        once: false,
+        duration: 1000,
+        offset: 100,
+        easing: 'ease-in-out-cubic'
+    });
+});
